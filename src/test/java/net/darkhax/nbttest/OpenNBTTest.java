0588b75c4cafdcdc9cf6cd5f13db25bdc7b78ff3
Darkhax <darklime@live.ca>
2016-03-09 12:08:28 -0700

    Allowed for multiple tests to be written and performed.

--- src/test/java/net/darkhax/nbttest/OpenNBTTest.java
+++ src/test/java/net/darkhax/nbttest/OpenNBTTest.java
@@ -7,7 +7,24 @@ import net.darkhax.opennbt.tags.CompoundTag;
 
 public class OpenNBTTest {
     
+    /**
+     * A string which represents the system specific newline character.
+     */
+    public static final String NEW_LINE = System.getProperty("line.separator");
+    
     public static void main (String[] args) {
+        
+        runIOTest();
+    }
+    
+    /**
+     * Performs a basic IO test. The goal of this test is to successfully write several
+     * properties and then read them back from the data. For the test to be considered a
+     * success, the output must be the same as the input.
+     */
+    private static void runIOTest () {
+        
+        System.out.println("Starting basic IO test");
         
         // Creates a new CompoundTag with various data stored.
         CompoundTag tag = new CompoundTag("TestTag");
@@ -21,11 +38,9 @@ public class OpenNBTTest {
         // Reads the NBT data from the NBTExample.nbt file.
         tag = NBTHelper.readFile("NBTExample.nbt");
         
-        // Prints the result of the tag's toString method to see if the data was written and
-        // read properly.
-        System.out.println("TestInteger: " + tag.getInt("TestInteger"));
-        System.out.println("TestString: " + tag.getString("TestString"));
-        System.out.println("TestIntegerArray: " + Arrays.toString(tag.getIntArray("TestIntegerArray")));
+        // Results for the test
+        System.out.println("TestInteger: " + tag.getInt("TestInteger") + NEW_LINE + "TestString: " + tag.getString("TestString") + NEW_LINE + "TestIntegerArray: " + Arrays.toString(tag.getIntArray("TestIntegerArray")));
         System.out.println("Tag Dump: " + tag.toString());
+        System.out.println("The IO test was " + ((tag.tagEquals("TestInteger", 1337) && tag.tagEquals("TestString", "Hello World!") && Arrays.equals(tag.getIntArray("TestIntegerArray"), new int[] { 200, 200, 208, 208, 203, 205, 203, 205, 48, 30 })) ? "successful!" : "not successful"));
     }
 }
