045b0679440e61c28264ec76e71eba7eddcc8729
Darkhax <darklime@live.ca>
2016-03-12 17:47:00 -0700

    Updated tests. Revealed issue in some tags being written.

--- src/test/java/net/darkhax/nbttest/OpenNBTTest.java
+++ src/test/java/net/darkhax/nbttest/OpenNBTTest.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 import net.darkhax.opennbt.NBTHelper;
 import net.darkhax.opennbt.tags.CompoundTag;
+import net.darkhax.opennbt.tags.StringTag;
 
 public class OpenNBTTest {
     
@@ -16,6 +17,8 @@ public class OpenNBTTest {
         
         runIOTest();
         runBooleanTest();
+        runCompleteTest();
+        runComplexTest();
     }
     
     /**
@@ -42,7 +45,7 @@ public class OpenNBTTest {
         // Results for the test
         System.out.println("TestInteger: " + tag.getInt("TestInteger") + NEW_LINE + "TestString: " + tag.getString("TestString") + NEW_LINE + "TestIntegerArray: " + Arrays.toString(tag.getIntArray("TestIntegerArray")));
         System.out.println("Tag Dump: " + tag.toString());
-        System.out.println("The IO test was " + ((tag.tagEquals("TestInteger", 1337) && tag.tagEquals("TestString", "Hello World!") && Arrays.equals(tag.getIntArray("TestIntegerArray"), new int[] { 200, 200, 208, 208, 203, 205, 203, 205, 48, 30 })) ? "successful!" : "not successful"));
+        System.out.println("The IO test was " + ((tag.tagEquals("TestInteger", 1337) && tag.tagEquals("TestString", "Hello World!") && Arrays.equals(tag.getIntArray("TestIntegerArray"), new int[] { 200, 200, 208, 208, 203, 205, 203, 205, 48, 30 })) ? "successful!" : "not successful") + NEW_LINE);
     }
     
     /**
@@ -69,6 +72,64 @@ public class OpenNBTTest {
         
         // Results for the test
         System.out.println("Tag Dump: " + tag.toString());
-        System.out.println("The IO test was " + ((tag.getBoolean("TestTrue") && !tag.getBoolean("TestFalse") && Arrays.equals(tag.getBooleanArray("TestBooleanArray"), new boolean[] { true, false, false, true })) ? "successful!" : "not successful!"));
+        System.out.println("The boolean test was " + ((tag.getBoolean("TestTrue") && !tag.getBoolean("TestFalse") && Arrays.equals(tag.getBooleanArray("TestBooleanArray"), new boolean[] { true, false, false, true })) ? "successful!" : "not successful!") + NEW_LINE);
+    }
+    
+    /**
+     * Performs an IO test on all of the non-complex tag types. The goal of the test is to
+     * write one tag of almost every type to another tag, and then see if it survives being
+     * written and read to the disk. This test does not check serializable objects,
+     * serializable object arrays and tag lists.
+     */
+    private static void runCompleteTest () {
+        
+        System.out.println("Starting basic complete test");
+        
+        // Sets all of the non-complex data to the test tag.
+        CompoundTag tag = new CompoundTag("TestTag");
+        tag.setByte("Byte", Byte.MIN_VALUE);
+        tag.setShort("Short", Short.MAX_VALUE);
+        tag.setInt("Int", Integer.MIN_VALUE + 100000);
+        tag.setLong("Long", Long.MAX_VALUE);
+        tag.setFloat("Float", Float.MIN_VALUE + 2.29034f);
+        tag.setDouble("Double", Double.MAX_VALUE - 22.9213d);
+        tag.setByteArray("ByteArray", new byte[] { Byte.MAX_VALUE, Byte.MAX_VALUE, Byte.MIN_VALUE });
+        tag.setString("String", "duivqe;idveu");
+        tag.setDoubleArray("DoubleArray", new double[] { Double.MAX_VALUE, Double.MIN_VALUE, Double.MAX_VALUE });
+        tag.setFloatArray("FloatArray", new float[] { 234f, 32f, 9432f });
+        tag.setLongArray("LongArray", new long[] { Long.MIN_VALUE, Long.MIN_VALUE, Long.MAX_VALUE });
+        tag.setShortArray("ShortArray", new short[] { Short.MAX_VALUE, Short.MIN_VALUE, Short.MIN_VALUE });
+        tag.setStringArray("StringArray", new String[] { "wefqreqgq", "ij3erfo2j", "feiioqvfl" });
+        
+        // Writes the tag to a new file called NBTExample.nbt
+        NBTHelper.writeFile(tag, "NBTExample.nbt");
+        
+        // Reads the NBT data from the NBTExample.nbt file.
+        CompoundTag readTag = NBTHelper.readFile("NBTExample.nbt");
+        
+        // A check to see if the two tags have the same data.
+        System.out.println("Initial Tag Dump: " + tag.toString());
+        System.out.println("Read Tag Dump: " + readTag.toString());
+        System.out.println("The basic complete test was " + ((tag.equals(readTag)) ? "successful!" : "not successful!") + NEW_LINE);
+    }
+    
+    private static void runComplexTest () {
+        
+        System.out.println("Starting complex complete test");
+        
+        // Sets all of the non-complex data to the test tag.
+        CompoundTag tag = new CompoundTag("TestTag");
+        tag.setTagList("TagList", Arrays.asList(new StringTag("Tag1", "Value1"), new StringTag("Tag2", "Value2"), new StringTag("Tag3", "Value3")));
+        
+        // Writes the tag to a new file called NBTExample.nbt
+        NBTHelper.writeFile(tag, "NBTExample.nbt");
+        
+        // Reads the NBT data from the NBTExample.nbt file.
+        CompoundTag readTag = NBTHelper.readFile("NBTExample.nbt");
+        
+        // A check to see if the two tags have the same data.
+        System.out.println("Initial Tag Dump: " + tag.toString());
+        System.out.println("Read Tag Dump: " + readTag.toString());
+        System.out.println("The Complex test was " + ((readTag.equals(tag)) ? "sucessful" : " not successful"));
     }
 }
